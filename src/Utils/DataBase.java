99ea7ab91eabaa31f4fc7ce7c4ea9a79f6d1bce1
sophalkim <sophal.kim.work@gmail.com>
2014-10-05 19:44:05 -0700

    Database class is coming along nicely

--- src/Utils/DataBase.java
+++ src/Utils/DataBase.java
@@ -2,6 +2,7 @@ package Utils;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class DataBase {
@@ -33,7 +34,7 @@ public class DataBase {
 		      String sql = "CREATE TABLE PRODUCT " +
 		                   "(ID INT PRIMARY KEY     NOT NULL," +
 		                   " NAME           TEXT    NOT NULL, " + 
-		                   " PRICE          INT     NOT NULL, " + 
+		                   " PRICE          FLOAT     NOT NULL, " + 
 		                   " IMAGE          INT)"; 
 		      stmt.executeUpdate(sql);
 		      stmt.close();
@@ -43,6 +44,22 @@ public class DataBase {
 		      System.exit(0);
 		    }
 		    System.out.println("Table created successfully");
+	}
+	
+	public static void deleteTable(String tableName) {
+		connect();
+		stmt = null;
+		try {
+		stmt = c.createStatement();
+	      String sql = "DROP TABLE " + tableName; 
+	      stmt.executeUpdate(sql);
+	      stmt.close();
+	      c.close();
+	    } catch ( Exception e ) {
+	      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+	      System.exit(0);
+	    }
+	    System.out.println("Table deleted successfully");
 	}
 	
 	public static void addProducts() {
@@ -75,6 +92,33 @@ public class DataBase {
 		      System.exit(0);
 		    }
 		    System.out.println("Records created successfully");
+	}
+	
+	public static void selectProducts() {
+		connect();
+		stmt = null;
+		try {
+		      stmt = c.createStatement();
+		      ResultSet rs = stmt.executeQuery( "SELECT * FROM PRODUCT;" );
+		      while ( rs.next() ) {
+		         int id = rs.getInt("id");
+		         String  name = rs.getString("name");
+		         int price  = rs.getInt("price");
+		         int image = rs.getInt("image");
+		         System.out.println( "ID = " + id );
+		         System.out.println( "NAME = " + name );
+		         System.out.println( "PRICE = " + price );
+		         System.out.println( "IMAGE = " + image );
+		         System.out.println();
+		      }
+		      rs.close();
+		      stmt.close();
+		      c.close();
+		    } catch ( Exception e ) {
+		      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+		      System.exit(0);
+		    }
+		    System.out.println("Operation done successfully");
 	}
 	
 	public static void main(String[] args) {
